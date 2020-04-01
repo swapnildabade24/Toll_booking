5b597fdb68e142d95cfe7682025501c580fd6b97
Swapnil Dabade <pyarelal@ksoftpl.com>
2020-04-01 21:24:18 +0530

    correction

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -16,7 +16,7 @@ class Admin
      */
     public function handle($request, Closure $next)
     {
-        if(Auth::user()->role->name == 'Admin') {
+        if(Auth::user()->role_id == 1) {
             return $next($request);    
         } else{
             return redirect('/login');
